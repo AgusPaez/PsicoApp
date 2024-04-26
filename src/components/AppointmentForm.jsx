f1158c748552f5fd608dbb250515cd0ecae1c8e3
AgusPaez <agus.123.paez@gmail.com>
2024-04-26 13:12:58 -0300

    edit appointment design

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 //Import Hook
 import { useForm } from 'react-hook-form';
+//images
+import imgAppointment from '../assets/images/imgAppointment.jpg';
 
 export const AppointmentForm = () => {
   const {
@@ -15,88 +17,109 @@ export const AppointmentForm = () => {
 
   return (
     <>
-      <div className="flex justify-center center mt-10">
-        <div className="m-4 bg-slate-500 border border-black h-auto w-2/5 rounded-3xl">
-          <h2 className="text-center  p-4">SOLICITAR CITAS</h2>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className=" p-6 m-6 flex flex-col "
-          >
-            <div className="flex">
-              <label className="m-2" htmlFor="nombre">
-                Nombre
-              </label>
+      <section className="h-[82vh] grid grid-cols-2 gap-10">
+        <div className="p-10">
+          <form onSubmit={handleSubmit(onSubmit)} className=" p-6 m-6">
+            <h2 className="text-lg ml-4">Pedir una cita</h2>
+            <div className="flex w-full my-4 gap-6">
+              <div className="w-2/5">
+                <input
+                  placeholder="Nombre"
+                  className="m-2 h-9 p-4 w-full rounded-2xl"
+                  id="nombre"
+                  {...register('nombre', { required: true })}
+                />
 
-              <input
-                // placeholder="Porfavor ingrese su nombre completo"
-                className="m-1 w-24 rounded-2xl"
-                id="nombre"
-                {...register('nombre', { required: true })}
-              />
-
-              {errors.nombre && errors.nombre.type === 'required' && (
-                <span className=" text-red-500 text-xs 0 ">
-                  Name is required
-                </span>
-              )}
-
-              <label className="m-2" htmlFor="apellido">
-                Apellido
-              </label>
-              <input
-                className="m-2"
-                id="apellido"
-                {...register('apellido', { required: true })}
-              />
-              {errors.nombre && errors.nombre.type === 'required' && (
-                <span className=" text-red-500 text-xs 0 ">
-                  Name is required
-                </span>
-              )}
+                {errors.nombre && errors.nombre.type === 'required' && (
+                  <span className=" text-red-500 text-xs m-2 ml-5 mt-0.5 absolute">
+                    El nombre es obligatorio
+                  </span>
+                )}
+              </div>
+              <div className="w-2/5">
+                <input
+                  placeholder="Apellido"
+                  className="m-2 rounded-2xl h-9 p-4 w-full"
+                  id="apellido"
+                  {...register('apellido', { required: true })}
+                />
+                {errors.apellido && errors.apellido.type === 'required' && (
+                  <span className=" text-red-500 text-xs m-2 ml-5 mt-0.5 absolute">
+                    El apellido es obligatorio
+                  </span>
+                )}
+              </div>
+              <div className="w-1/5">
+                <input
+                  placeholder="Edad"
+                  className="m-2 h-9 p-4 w-full rounded-2xl"
+                  id="edad"
+                  {...register('edad', { required: true })}
+                />
+                {errors.edad && errors.edad.type === 'required' && (
+                  <span className=" text-red-500 text-xs m-2 mt-0.5 absolute">
+                    La edad es obligatoria
+                  </span>
+                )}
+              </div>
             </div>
-            <div className="flex">
-              <label className="m-2 w-1/2" htmlFor="nombre">
-                Edad
-              </label>
-
+            <div className="">
               <input
-                className="m-2"
-                id="nombre"
-                {...register('nombre', { required: true })}
-              />
-            </div>
-            <div className="flex">
-              <label className="m-2 w-1/2" htmlFor="nombre">
-                Motivo de consulta:
-              </label>
-
-              <input
-                className="m-2"
-                id="nombre"
-                {...register('nombre', { required: true })}
+                placeholder="Email"
+                className="m-2 my-4 h-9 p-4 w-full rounded-2xl"
+                id="email"
               />
             </div>
-            <div className="flex">
-              <label className="m-2 w-1/2" htmlFor="nombre">
-                Derivacion o recomendacion de:
-              </label>
+            <div className="flex w-full my-4 gap-6">
+              <div className="w-1/2">
+                <input
+                  placeholder="Numero de telefono"
+                  className="m-2 h-9 p-4 w-full rounded-2xl"
+                  id="numero"
+                  {...register('numero', { required: true })}
+                />
+                {errors.numero && errors.numero.type === 'required' && (
+                  <span className=" text-red-500 text-xs m-2 mt-0.5 absolute ml-4 ">
+                    El número es obligatorio
+                  </span>
+                )}
+              </div>
 
-              <input
-                className="m-2"
-                id="nombre"
-                {...register('nombre', { required: true })}
+              <div className="w-1/2">
+                <input
+                  placeholder="Derivacion"
+                  className="m-2 h-9 p-4 w-full rounded-2xl"
+                  id="derivacion"
+                />
+              </div>
+            </div>
+            <div className="my-6">
+              <textarea
+                placeholder="Motivo de consulta"
+                className="m-2 max-h-44 min-h-20 p-4 w-full rounded-2xl text-left"
+                id="motivo_consulta"
+                {...register('motivo_consulta', { required: true })}
               />
+              {errors.motivo_consulta &&
+                errors.motivo_consulta.type === 'required' && (
+                  <span className=" text-red-500 text-xs m-2 mt-0.5 absolute ml-4">
+                    El motivo consulta es obligatorio
+                  </span>
+                )}
             </div>
 
             <button
-              className="m-2 bg-slate-400 hover:bg-slate-600"
+              className="m-2 h-12 w-32 bg-slate-400 hover:bg-slate-600 rounded-full"
               type="submit"
             >
-              ENVIAR
+              Enviar
             </button>
           </form>
         </div>
-      </div>
+        <div className="">
+          <img src={imgAppointment} alt="imagen" width={200} height={200} />
+        </div>
+      </section>
     </>
   );
 };
