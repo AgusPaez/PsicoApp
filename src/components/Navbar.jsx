203bc7eaee3dc3f833ed52483e632f80dc9a5a1f
AgusPaez <agus.123.paez@gmail.com>
2024-08-08 21:36:46 -0300

    edit and order components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+//Icons
+import MenuIcon from '../assets/icons/MenuIcon';
 //images
 import SymbolPsico from '../assets/icons/pngwing.com.png';
+import NotificationsIcon from '../assets/icons/NotificationsIcon';
 
 export const Navbar = () => {
   const location = useLocation();
   const isActive = (path) => location.pathname === path;
+  //States
   const [openMenu, setOpenMenu] = useState(false);
   const [optionsProfile, setOptionsProfile] = useState(false);
   const buttonClass = (path) => `${isActive(path) ? 'bg-gray-700' : ''} 
 `;
-
+  //open menu function
   const showMenu = () => {
     setOpenMenu(!openMenu);
   };
@@ -34,30 +38,9 @@ export const Navbar = () => {
               >
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
-                {/* <!--
-            Icon when menu is closed.
 
-            Menu open: "hidden", Menu closed: "block"
-          --> */}
-                <svg
-                  className="block h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-                {/* <!--
-            Icon when menu is open.
+                <MenuIcon />
 
-            Menu open: "block", Menu closed: "hidden"
-          --> */}
                 <svg
                   className="hidden h-6 w-6"
                   fill="none"
@@ -134,20 +117,7 @@ export const Navbar = () => {
               >
                 <span className="absolute -inset-1.5"></span>
                 <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
+                <NotificationsIcon />
               </button>
               {/* END Notifications */}
               {/* Profile  */}
--- src/components/Psico-components/LayoutStudies.jsx
+++ src/components/Psico-components/LayoutStudies.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { fetchAllStudies } from '../../services/StudiesService';
 export const LayoutStudies = () => {
+  //state use for studies
+  const [studies, setStudies] = useState([]);
+  //fetch studies
+  useEffect(() => {
+    const fetchStudies = async () => {
+      try {
+        // call service
+        const studies = await fetchAllStudies();
+        // update studies
+        setStudies(studies);
+      } catch (error) {
+        console.error('Error fetching studies', error);
+      }
+    };
+    fetchStudies();
+  }, []);
+  console.log(studies); //para corroborar resultados
   return (
     <form className="bg-black text-gray-400 p-5 my-12 m-8 flex flex-col justify-center rounded-full  ">
       <label className="flex flex-col" htmlFor="">
@@ -10,7 +27,7 @@ export const LayoutStudies = () => {
       <label className="flex flex-col" htmlFor="">
         Formacion
       </label>
-      <input></input>
+
       <label className="flex flex-col" htmlFor="">
         Imagenes
       </label>
