33e1d7350980be6babb3df21e28528e7c07a0a34
AgusPaez <agus.123.paez@gmail.com>
2024-08-09 18:28:21 -0300

    show studies in panel

--- src/components/Psico-components/LayoutStudies.jsx
+++ src/components/Psico-components/LayoutStudies.jsx
@@ -18,20 +18,37 @@ export const LayoutStudies = () => {
     fetchStudies();
   }, []);
   console.log(studies); //para corroborar resultados
-  return (
-    <form className="bg-black text-gray-400 p-5 my-12 m-8 flex flex-col justify-center rounded-full  ">
-      <label className="flex flex-col" htmlFor="">
-        Nombre del/la Psicologo/a
-      </label>
-      <input></input>
-      <label className="flex flex-col" htmlFor="">
-        Formacion
-      </label>
 
-      <label className="flex flex-col" htmlFor="">
-        Imagenes
-      </label>
-      <input></input>
-    </form>
+  const OpenEdit = (studies) => {
+    console.log(studies);
+  };
+
+  return (
+    // <form className="bg-black text-gray-400 p-5 my-12 m-8 flex flex-col justify-center rounded-full  ">
+    //   <label className="flex flex-col" htmlFor="">
+    //     Nombre del/la Psicologo/a
+    //   </label>
+    //   <input></input>
+    //   <label className="flex flex-col" htmlFor="">
+    //     Formacion
+    //   </label>
+
+    //   <label className="flex flex-col" htmlFor="">
+    //     Imagenes
+    //   </label>
+    //   <input></input>
+    // </form>
+    <>
+      <ul className="bg-black text-gray-400 p-14 my-12 m-8 flex flex-col rounded-full gap-3 ">
+        {studies.map((studies) => (
+          <li key={studies._id} className="">
+            {' '}
+            {studies.titulo}, {studies.institucion}, ({studies.anio}),
+            <button onClick={() => OpenEdit(studies)}>Editar</button>
+            <button>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
