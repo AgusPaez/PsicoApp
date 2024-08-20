dbdfd1cd0063278603389285eb128950abaacda2
AgusPaez <agus.123.paez@gmail.com>
2024-08-20 18:55:43 -0300

    show datalogin in patient

--- src/pages/Patient/HomePatient.jsx
+++ src/pages/Patient/HomePatient.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { useAuth } from '../../context/AuthProvider';
 export const HomePatient = () => {
-  const { login, isAuthenticated, user, logout } = useAuth();
+  const { login, isAuthenticated, user, logout, dataLogin } = useAuth();
+  console.log(dataLogin);
   return (
     <div>
       HomePatient
+      <div>
+        datos de la sesion iniciada "paciente"
+        <div>
+          <h1>
+            NOMBRE: {dataLogin.nombre}, {dataLogin.apellido}
+          </h1>
+          <h1>EMAIL ASOCIADO : {dataLogin.email}</h1>
+          <h1> Cuenta registrada el : {dataLogin.createdAt}</h1>
+          <h1></h1>
+        </div>
+      </div>
       <button onClick={logout}>SALIR</button>
     </div>
   );
