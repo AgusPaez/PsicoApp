e16f9973c128ba43b2c9c438a83fa2e97b6bff93
AgusPaez <agus.123.paez@gmail.com>
2024-10-01 14:21:55 -0300

    edit style + add icon

--- src/components/Psico-components/LayoutPatients.jsx
+++ src/components/Psico-components/LayoutPatients.jsx
@@ -1,7 +1,10 @@
 //imports
 import React, { useEffect, useState } from 'react';
+//import Icons
+import { PatientsICON } from '../../assets/icons/PatientsICON';
 //import components
 import { EditPatients } from './EditPatients';
+import { AddPatientModal } from './AddPatientModal';
 import { AddPatient } from './AddPatient';
 //import services
 import { findPatients, deleteProfile } from '../../services/users';
@@ -48,40 +51,89 @@ export const LayoutPatients = () => {
 
   return (
     <>
-      <section>
-        <div>Pacientes Registrados</div>
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Apellido</th>
-              <th>Email</th>
-              <th>Numero</th>
-              <th>Fecha Nacimiento</th>
-              <th>O.S</th>
-              <th>Creada</th>
-            </tr>
-          </thead>
-          <tbody>
-            {patients.map((patients, index) => (
-              <tr key={index}>
-                <td>{patients.nombre}</td>
-                <td>{patients.apellido}</td>
-                <td>{patients.email}</td>
-                <td>{patients.numero}</td>
-                <td>{patients.fecha_nacimiento.substring(0, 10)}</td>
-                <td>{patients.obra_social}</td>
-                <td>{patients.createdAt.substring(0, 10)}</td>
-                <td>
-                  <button onClick={() => OpenEdit(patients)}>Editar</button>
-                </td>
-                <td></td>
+      <section className="flex flex-col w-5/6 p-5 m-5 ml-10 border border-blue-500 rounded-xl">
+        <div className="flex items-center justify-center gap-4 p-2 m-4 text-white ">
+          <PatientsICON h={40} w={40} color={'#edf0ea'} />
+          <h2 className="font-semibold text-black ">
+            EDICION SECCION: PACIENTES
+          </h2>
+          <PatientsICON h={40} w={40} color={'#edf0ea'} />
+        </div>
+        <div className="overflow-y-auto h-5/6 scrollbar scrollbar-thumb-blue-500 scrollbar-track-slate-600">
+          <table className="min-w-full overflow-hidden text-gray-300 bg-gray-800 rounded-md shadow-lg table-auto bg-opacity-30">
+            <thead className="">
+              <tr className="text-sm font-semibold tracking-wide text-gray-200 uppercase bg-gray-700 bg-opacity-40">
+                <th className="px-2 py-3 text-left">Nombre</th>
+                <th className="px-2 py-3 text-left">Apellido</th>
+                <th className="px-2 py-3 text-left"> D.N.I. </th>
+                <th className="px-2 py-3 text-left">Email</th>
+                <th className="px-2 py-3 text-left">Número</th>
+                {/* <th className="px-2 py-3 text-left">Fecha Nacimiento</th> */}
+                <th className="px-2 py-3 text-left">O.S</th>
+
+                {/* <th className="px-2 py-3 text-left">Creada</th> */}
+                {/* <th className="px-2 py-3 text-left">Acciones</th> */}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {patients.map((patient, index) => (
+                <tr
+                  key={index}
+                  className="transition-colors duration-200 cursor-pointer odd:bg-gray-800 odd:bg-opacity-40 hover:bg-gray-700 "
+                  onClick={() => OpenEdit(patient)}
+                >
+                  <td className="px-2 py-2 text-sm border-b border-gray-700">
+                    {patient.nombre}
+                  </td>
+                  <td className="px-2 py-2 text-sm border-b border-gray-700">
+                    {patient.apellido}
+                  </td>
+                  <td className="px-2 py-2 text-sm border-b border-gray-700">
+                    {patient.dni}
+                  </td>
+                  <td className="px-2 py-2 text-sm border-b border-gray-700">
+                    {patient.email}
+                  </td>
+                  <td className="px-2 py-2 text-sm border-b border-gray-700">
+                    {patient.numero}
+                  </td>
+                  {/* <td className="px-2 py-2 text-sm border-b border-gray-700">
+                    {patient.fecha_nacimiento.substring(0, 10)}
+                  </td> */}
+                  <td className="px-2 py-2 text-sm border-b border-gray-700">
+                    {patient.obra_social}
+                  </td>
+                  {/* <td className="px-2 py-2 text-sm border-b border-gray-700">
+                  {patient.createdAt.substring(0, 10)}
+                </td> */}
+                  {/* <td className="px-2 py-2 text-center border-b border-gray-700">
+                  <button
+                    onClick={() => OpenEdit(patient)}
+                    className="px-2 py-1 text-xs text-white transition-colors duration-200 bg-blue-600 rounded-lg hover:bg-blue-500"
+                  >
+                    Editar
+                  </button>
+                </td> */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <section>
+          <div className="flex w-full my-4">
+            <button
+              className="text-center py-1 px-2 mx-auto text-[#0084f0] hover:font-semibold hover:text-[#64b9ff] animate-pulse ease-in-out transition-all duration-500 hover:tracking-widest hover:scale-110"
+              onClick={OpenAdd}
+            >
+              AGREGAR PACIENTE
+            </button>
+          </div>
+          {/* <div>{openAdd && <AddPatient close={OpenAdd} />}</div> */}
+        </section>
       </section>
       <div>
+        {openAdd && <AddPatientModal onClose={OpenAdd} admin={true} />}
         {openEdit && (
           <EditPatients
             close={OpenEdit}
@@ -90,10 +142,6 @@ export const LayoutPatients = () => {
           />
         )}
       </div>
-      <section>
-        <button onClick={OpenAdd}>Agregar paciente</button>
-        <div>{openAdd && <AddPatient close={OpenAdd} />}</div>
-      </section>
     </>
   );
 };
