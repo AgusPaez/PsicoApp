0b4f7aa052c504ea2abd85fbbeb138c523c2d9b5
AgusPaez <agus.123.paez@gmail.com>
2024-10-04 14:08:01 -0300

    changue background

--- src/pages/Patient/AboutMePatient.jsx
+++ src/pages/Patient/AboutMePatient.jsx
@@ -8,9 +8,19 @@ import { AboutMePatientComponent } from '../../components/Patient-components/Abo
 export const AboutMePatient = () => {
   return (
     <>
-      <NavbarPatient />
-      <AboutMePatientComponent />
-      <FooterPatient />
+      <div className="relative min-h-screen bg-[#f5f0e1] text-[#4a4a4a]">
+        {/* Imagen de fondo */}
+        <div
+          className="absolute inset-0 bg-center bg-cover opacity-30 blur-[2px]"
+          style={{
+            backgroundImage:
+              "url('https://img.freepik.com/vector-gratis/hierbas-botanicas-minimalistas-flores-silvestres-estilo-vintage_23-2148424504.jpg')",
+          }}
+        ></div>
+        <NavbarPatient />
+        <AboutMePatientComponent />
+        <FooterPatient />
+      </div>
     </>
   );
 };
--- src/pages/Patient/HomePatient.jsx
+++ src/pages/Patient/HomePatient.jsx
@@ -8,9 +8,19 @@ import { FooterPatient } from '../../components/Patient-components/FooterPatient
 export const HomePatient = () => {
   return (
     <>
-      <NavbarPatient />
-      <HomePatientComponent />
-      <FooterPatient />
+      <div className="relative min-h-screen bg-[#f5f0e1] text-[#4a4a4a]">
+        {/* Imagen de fondo */}
+        <div
+          className="absolute inset-0 bg-center bg-cover opacity-30 blur-[2px]"
+          style={{
+            backgroundImage:
+              "url('https://img.freepik.com/vector-gratis/hierbas-botanicas-minimalistas-flores-silvestres-estilo-vintage_23-2148424504.jpg')",
+          }}
+        ></div>
+        <NavbarPatient />
+        <HomePatientComponent />
+        <FooterPatient />
+      </div>
     </>
   );
 };
