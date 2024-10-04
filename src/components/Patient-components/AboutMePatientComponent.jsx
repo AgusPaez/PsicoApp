4ff8a9781405a9f591648fd88a7a20e27e868b44
AgusPaez <agus.123.paez@gmail.com>
2024-10-04 14:07:24 -0300

    edit style

--- src/components/Patient-components/AboutMePatientComponent.jsx
+++ src/components/Patient-components/AboutMePatientComponent.jsx
@@ -1,118 +1,154 @@
-//imports
+// imports
 import React, { useEffect, useState } from 'react';
-//import components
+// import components
 import { EditMyProfile } from './EditMyProfile';
-//import service
+// import service
 import { findAll } from '../../services/appointmentService';
-//import context
+// import context
 import { useAuth } from '../../context/AuthProvider';
 
 export const AboutMePatientComponent = () => {
-  //context
+  // context
   const { dataLogin } = useAuth();
-  //states
-  const [appointment, setAppointment] = useState([]);
+  // states
+  const [appointments, setAppointments] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    const fetchAppointment = async () => {
+    const fetchAppointments = async () => {
       try {
-        //call service
+        // call service
         const response = await findAll();
-        setAppointment(response);
-        console.log('se pudo traer las consultas', response);
+        setAppointments(response);
+        console.log('Se pudieron traer las consultas', response);
       } catch (error) {
-        console('error al traer las consultas', error);
+        console.error('Error al traer las consultas', error);
       }
     };
-    fetchAppointment();
+    fetchAppointments();
   }, []);
-  //show appointment
-  const appointmentFill = appointment.filter(
+
+  // Filter appointments for the logged-in patient
+  const patientAppointments = appointments.filter(
     (appointment) => appointment.email === dataLogin.email
   );
 
-  //Show edit modal
-  const ShowModal = () => {
+  // Get the last appointment (most recent)
+  const lastAppointment =
+    patientAppointments.length > 0
+      ? patientAppointments.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        )[0]
+      : null;
+
+  // Show edit modal
+  const toggleModal = () => {
     setShowModal(!showModal);
   };
 
   return (
     <>
-      <section>
-        <div>
-          <h1>Perfil del Paciente</h1>
-          <p>
-            <strong>Nombre:</strong> {dataLogin.nombre}
-          </p>
-          <p>
-            <strong>Apellido:</strong> {dataLogin.apellido}
-          </p>
-          <p>
-            <strong>Email:</strong> {dataLogin.email}
-          </p>
-          <p>
-            <strong>Numero de telefono:</strong> {dataLogin.numero}
-          </p>
-          <p>
-            <strong>Fecha de Nacimiento:</strong> {dataLogin.fecha_nacimiento}
-          </p>
-          <p>
-            <strong>Obra social:</strong> {dataLogin.obra_social}
-          </p>
-          <p>
-            <strong>Imagen :</strong> {dataLogin.imagenUrl || 'No disponible'}
-          </p>
-          <p>
-            <strong>Creado el:</strong>{' '}
-            {new Date(dataLogin.createdAt).toLocaleString()}
-          </p>
-          <p>
-            <strong>Actualizado el:</strong>{' '}
-            {new Date(dataLogin.updatedAt).toLocaleString()}
-          </p>
+      <section className="relative z-40 flex p-6 space-x-32 justify-stretch mx-28 justify-items-start opacity-90">
+        <div className="bg-[#dad4c4] bg-opacity-65 border border-slate-400 rounded-lg shadow-lg ">
+          <div className="p-6">
+            <h1 className="mb-4 text-2xl font-bold">
+              Perfil de {dataLogin.email}
+            </h1>
+            <div className="space-y-2">
+              <p>
+                <strong>Imagen:</strong>{' '}
+                <img
+                  src={dataLogin.imagenUrl}
+                  alt="Perfil"
+                  className="w-24 h-24 rounded-full"
+                />
+              </p>
+              <p>
+                <strong>Nombre:</strong> {dataLogin.nombre}
+              </p>
+              <p>
+                <strong>Apellido:</strong> {dataLogin.apellido}
+              </p>
+              <p>
+                <strong>Email:</strong> {dataLogin.email}
+              </p>
+              <p>
+                <strong>Número de Teléfono:</strong> {dataLogin.numero}
+              </p>
+              <p>
+                <strong>Fecha de Nacimiento:</strong>{' '}
+                {dataLogin.fecha_nacimiento.split('T')[0]}
+              </p>
+              <p>
+                <strong>Obra Social:</strong> {dataLogin.obra_social}
+              </p>
 
-          <button className="bg-slate-600" onClick={ShowModal}>
-            Actualizar perfil
-          </button>
-          {showModal && (
-            <EditMyProfile profile={dataLogin} onClose={ShowModal} />
-          )}
-          <div className="mt-6 mb-auto">
-            <h1>Citas del Paciente ({dataLogin.email})</h1>
-            {appointmentFill.length > 0 ? (
-              <ul>
-                {appointmentFill.slice(0, 1).map((cita) => (
-                  <li key={cita._id}>
-                    <p>
-                      <strong>Nombre:</strong> {cita.nombre} {cita.apellido}
-                    </p>
-                    <p>
-                      <strong>Edad:</strong> {cita.edad}
-                    </p>
-                    <p>
-                      <strong>Motivo de Consulta:</strong>{' '}
-                      {cita.motivo_consulta}
-                    </p>
-                    <p>
-                      <strong>Derivación:</strong> {cita.derivacion}
-                    </p>
-                    <p>
-                      <strong>Número:</strong> {cita.numero}
-                    </p>
-                    <p>
-                      <strong>Email:</strong> {cita.email}
-                    </p>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>No hay citas para este paciente.</p>
+              <p>
+                <strong>Creado el:</strong>{' '}
+                {new Date(dataLogin.createdAt).toLocaleString()}
+              </p>
+              <p>
+                <strong>Actualizado el:</strong>{' '}
+                {new Date(dataLogin.updatedAt).toLocaleString()}
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <button
+                className="px-4 py-2 mt-4 text-white transition-all duration-300 rounded bg-slate-600 hover:tracking-widest hover:bg-slate-700"
+                onClick={toggleModal}
+              >
+                Ver/Modificar perfil
+              </button>
+            </div>
+            {showModal && (
+              <EditMyProfile profile={dataLogin} onClose={toggleModal} />
             )}
           </div>
         </div>
-        datos sobre el perfil (citas, datos personales, cambiar la contraseña ,
-        eliminar cuenta)
+
+        <div className="w-1/2 mt-6 ">
+          <h2 className="mb-2 text-xl font-semibold">
+            Última Cita del Paciente ({dataLogin.email})
+          </h2>
+          {lastAppointment ? (
+            <div className="p-4  border rounded bg-[#dad4c4] bg-opacity-65  border-slate-400">
+              <p>
+                <strong>Nombre:</strong> {lastAppointment.nombre}{' '}
+                {lastAppointment.apellido}
+              </p>
+              <p>
+                <strong>Edad:</strong> {lastAppointment.edad}
+              </p>
+              <p>
+                <strong>Motivo de Consulta:</strong>{' '}
+                {lastAppointment.motivo_consulta}
+              </p>
+              <p>
+                <strong>Derivación:</strong> {lastAppointment.derivacion}
+              </p>
+              <p>
+                <strong>Número:</strong> {lastAppointment.numero}
+              </p>
+              <p>
+                <strong>Email:</strong> {lastAppointment.email}
+              </p>
+              <p>
+                <strong>Fecha de Consulta:</strong>{' '}
+                {new Date(lastAppointment.createdAt).toLocaleString()}
+              </p>
+              <div className="flex justify-between mt-6">
+                <button className="px-4 py-2 text-white transition-all duration-300 rounded bg-slate-600 hover:tracking-widest hover:bg-slate-700">
+                  Ver Todas las Citas
+                </button>
+                <button className="px-4 py-2 text-white transition-all duration-300 bg-green-700 rounded hover:tracking-widest hover:bg-green-800">
+                  Pedir una Cita
+                </button>
+              </div>
+            </div>
+          ) : (
+            <p>No has solicitado citas.</p>
+          )}
+        </div>
       </section>
     </>
   );
--- src/components/Patient-components/HomePatientComponent.jsx
+++ src/components/Patient-components/HomePatientComponent.jsx
@@ -1,30 +1,101 @@
-// imports
-import React from 'react';
-//import context
+//imports
+import React, { useEffect, useRef } from 'react';
 import { useAuth } from '../../context/AuthProvider';
+import { Link } from 'react-router-dom';
+// import videoSrc from '../../assets/videos/vecteezy_continuous-line-hand-drawn-beautiful-black-and-white-line_6554042.mp4';
 
 export const HomePatientComponent = () => {
-  //context
-  const { login, isAuthenticated, user, logout, dataLogin } = useAuth();
+  // Context
+  const { dataLogin, logout } = useAuth();
+  const videoRef = useRef(null); // Crear una referencia para el video
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.5;
+    }
+  }, []);
+
   return (
     <>
-      <div>
-        HomePatient
-        <div>
-          datos de la sesion iniciada "paciente"
-          <div>
-            <h1>
-              NOMBRE: {dataLogin.nombre}, {dataLogin.apellido}
-            </h1>
-            <h1>EMAIL ASOCIADO : {dataLogin.email}</h1>
-            <h1>
-              {' '}
-              Cuenta registrada el : {dataLogin.createdAt.substring(0, 10)}
-            </h1>
-            <h1></h1>
-          </div>
+      {/* Contenido principal */}
+      <div className="relative z-10 p-10 px-32">
+        {/* Cabecera de bienvenida */}
+        <div className="bg-[#dad4c4] bg-opacity-65 rounded-lg shadow-lg p-6 mb-8 ">
+          <h1 className="text-4xl font-bold text-center mb-4 text-[#4a4a4a]">
+            Bienvenido, {dataLogin.nombre} {dataLogin.apellido}
+          </h1>
+          <p className="text-center text-lg text-[#7a7a7a]">
+            Estamos encantados de verte de nuevo. Aquí puedes gestionar tus
+            citas, perfil y más.
+          </p>
         </div>
-        {/* <button onClick={logout}>SALIR</button> */}
+
+        {/* Información del paciente */}
+        <div className="bg-[#dad4c4] bg-opacity-70 rounded-lg shadow-lg p-6 mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-[#4a4a4a] ">
+            Información de tu cuenta
+          </h2>
+          <p className="mb-2 text-lg">
+            <strong>Email asociado:</strong> {dataLogin.email}
+          </p>
+          <p className="text-lg">
+            <strong>Cuenta registrada el:</strong>{' '}
+            {dataLogin.createdAt.substring(0, 10)}
+          </p>
+          {/* <div className="flex items-center justify-center bg-transparent h-14 rounded-[100px]">
+            <video
+              loop
+              autoPlay
+              muted // Para que no reproduzca audio automáticamente
+              ref={videoRef} // Asignar la referencia al video
+              className="w-20 max-w-2xl rounded-[100px] shadow-lg"
+              src={videoSrc}
+            >
+              Your browser does not support the video tag.
+            </video>
+          </div> */}
+        </div>
+
+        {/* Acciones y botones */}
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {/* Botones con enlaces */}
+          <Link
+            to="/AppointmentPatient"
+            className="block bg-[#a1968d] hover:bg-[#8d7e73] text-[#f5f0e1] text-center font-semibold hover:font-extrabold py-4 px-6 rounded-lg shadow-md hover:tracking-widest transition-all duration-300"
+          >
+            Ver tus citas
+          </Link>
+          <Link
+            to="/AboutMePatient"
+            className="block bg-[#a1968d] hover:bg-[#8d7e73] text-[#f5f0e1] text-center font-semibold hover:font-extrabold py-4 px-6 rounded-lg shadow-md hover:tracking-widest transition-all duration-300"
+          >
+            Editar perfil
+          </Link>
+          <Link
+            to="/*"
+            className="block bg-[#a1968d] hover:bg-[#8d7e73] text-[#f5f0e1] text-center font-semibold hover:font-extrabold py-4 px-6 rounded-lg shadow-md hover:tracking-widest transition-all duration-300"
+          >
+            Recursos y guías
+          </Link>
+          <Link
+            to="/*"
+            className="block bg-[#a1968d] hover:bg-[#8d7e73] text-[#f5f0e1] text-center font-semibold hover:font-extrabold py-4 px-6 rounded-lg shadow-md hover:tracking-widest transition-all duration-300"
+          >
+            Soporte técnico
+          </Link>
+          <Link
+            to="/AboutMePatient"
+            className="block bg-[#a1968d] hover:bg-[#8d7e73] text-[#f5f0e1] text-center font-semibold hover:font-extrabold py-4 px-6 rounded-lg shadow-md hover:tracking-widest transition-all duration-300"
+          >
+            Configuración de la cuenta
+          </Link>
+          <Link
+            onClick={logout}
+            className="block bg-[#4a4a4a] hover:bg-[#2f2f2f] text-[#f5f0e1] text-center font-semibold hover:font-extrabold py-4 px-6 rounded-lg shadow-md hover:tracking-widest transition-all duration-300"
+          >
+            Cerrar sesión
+          </Link>
+        </div>
       </div>
     </>
   );
