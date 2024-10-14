9c495c2321d24f0590342f6dabe8facdb0505796
AgusPaez <agus.123.paez@gmail.com>
2024-10-14 18:37:23 -0300

    add imgs

--- src/pages/Main/AboutMe.jsx
+++ src/pages/Main/AboutMe.jsx
@@ -5,15 +5,24 @@ import { BackgroundMain } from '../../components/Backgrounds/BackgroundMain';
 import { Navbar } from '../../components/Navbar';
 import { Footer } from '../../components/Footer';
 import { AboutmeComponent } from '../../components/AboutmeComponent';
+// import Fondo from '../../assets/images/fondo.jpg';
+import Aboutme from '../../assets/images/aboutme.jpg';
 
 export const AboutMe = () => {
   return (
     <>
-      <BackgroundMain>
-        <Navbar />
-        <AboutmeComponent />
-        <Footer />
-      </BackgroundMain>
+      <div className="relative z-0 min-h-screen">
+        {/* Imagen de fondo */}
+        <div
+          className="absolute inset-0 bg-center bg-cover opacity-30 blur-[2px] z-0"
+          style={{ backgroundImage: `url(${Aboutme})` }}
+        ></div>
+        <BackgroundMain>
+          <Navbar />
+          <AboutmeComponent />
+          <Footer />
+        </BackgroundMain>
+      </div>
     </>
   );
 };
--- src/pages/Main/Appointment.jsx
+++ src/pages/Main/Appointment.jsx
@@ -6,15 +6,24 @@ import { BackgroundMain } from '../../components/Backgrounds/BackgroundMain';
 import { Navbar } from '../../components/Navbar';
 import { Footer } from '../../components/Footer';
 import { AppointmentForm } from '../../components/AppointmentForm';
+import Fondo from '../../assets/images/fondo.jpg';
 
 export const Appointment = () => {
   return (
     <>
-      <BackgroundMain>
-        <Navbar></Navbar>
-        <AppointmentForm></AppointmentForm>
-        <Footer></Footer>
-      </BackgroundMain>
+      <div className="relative z-0">
+        {/* Imagen de fondo */}
+        <div
+          className="absolute inset-0 bg-center bg-cover opacity-60 blur-[2.5px] z-0"
+          style={{ backgroundImage: `url(${Fondo})` }}
+        ></div>
+
+        <BackgroundMain>
+          <Navbar></Navbar>
+          <AppointmentForm></AppointmentForm>
+          <Footer></Footer>
+        </BackgroundMain>
+      </div>
     </>
   );
 };
