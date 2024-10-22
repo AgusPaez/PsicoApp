2a51bfef07b53027ec325030b411c5e7e65d7518
AgusPaez <agus.123.paez@gmail.com>
2024-10-22 12:47:59 -0300

    add spinner, inputs + changue styles

--- src/components/Psico-components/EditMyProfile.jsx
+++ src/components/Psico-components/EditMyProfile.jsx
@@ -5,8 +5,13 @@ import { useForm } from 'react-hook-form';
 import { updateMyProfile, deleteProfile } from '../../services/users';
 // Import context
 import { useAuth } from '../../context/AuthProvider';
+// Import spinner
+import { LoadingSpinner } from '../LoadingSpinner';
 
 export const EditMyProfile = ({ profile, onClose }) => {
+  //states
+  const [loading, setLoading] = useState(false);
+  //rhf
   const {
     register,
     handleSubmit,
@@ -16,10 +21,12 @@ export const EditMyProfile = ({ profile, onClose }) => {
   const [previewImage, setPreviewImage] = useState('');
   const { logout } = useAuth();
 
+  //set values
   useEffect(() => {
     if (profile) {
       setValue('nombre', profile.nombre || '');
       setValue('apellido', profile.apellido || '');
+      setValue('dni', profile.dni || '');
       setValue('email', profile.email || '');
       setValue('rol', profile.rol || '');
       setValue('numero', profile.numero || '');
@@ -32,6 +39,7 @@ export const EditMyProfile = ({ profile, onClose }) => {
     }
   }, [profile, setValue]);
 
+  //handle image function
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -40,7 +48,9 @@ export const EditMyProfile = ({ profile, onClose }) => {
     }
   };
 
+  //handle submit
   const onSubmit = async (data) => {
+    setLoading(true);
     try {
       const formData = new FormData();
       Object.keys(data).forEach((key) => formData.append(key, data[key]));
@@ -48,14 +58,20 @@ export const EditMyProfile = ({ profile, onClose }) => {
       if (data.imagen) {
         formData.append('imagen', data.imagen);
       }
-
-      const updatedProfile = await updateMyProfile(profile._id, formData);
-      console.log('Profile updated successfully:', updatedProfile);
+      setTimeout(async () => {
+        //call service
+        const updatedProfile = await updateMyProfile(profile._id, formData);
+        console.log('Profile updated successfully:', updatedProfile);
+        setLoading(false);
+      }, 4000);
     } catch (error) {
       console.error('Failed to update profile:', error);
+      setTimeout(() => {
+        setLoading(false);
+      }, 3500);
     }
   };
-
+  //destroy my profile function
   const deleteMyProfile = async () => {
     try {
       await deleteProfile(profile._id);
@@ -77,70 +93,97 @@ export const EditMyProfile = ({ profile, onClose }) => {
             className="object-cover w-32 h-32 mx-auto mb-4 rounded-full"
           />
         )}
+        <label className="pl-4 text-sm"> Imagen de perfil </label>
         <input
           type="file"
           accept="image/*"
           onChange={handleImageChange}
-          className="cursor-pointer w-full p-4 m-2 text-left min-h-12 transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
+          className="cursor-pointer w-full p-4 mx-2 text-left min-h-12 transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
         />
-
-        <input
-          type="text"
-          placeholder="Nombre"
-          {...register('nombre', { required: true })}
-          className="w-full p-4 m-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
-        />
-        {errors.nombre && (
-          <span className="text-red-500">El nombre es obligatorio</span>
-        )}
-
-        <input
-          type="text"
-          placeholder="Apellido"
-          {...register('apellido', { required: true })}
-          className="w-full p-4 m-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
-        />
-        {errors.apellido && (
-          <span className="text-red-500">El apellido es obligatorio</span>
-        )}
-
-        <input
-          type="email"
-          placeholder="Email"
-          {...register('email', { required: true })}
-          className="w-full p-4 m-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
-        />
-        {errors.email && (
-          <span className="text-red-500">El email es obligatorio</span>
-        )}
-
-        <input
-          type="text"
-          placeholder="Número de Teléfono"
-          {...register('numero', { required: true })}
-          className="w-full p-4 m-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
-        />
-        {errors.numero && (
-          <span className="text-red-500">El número es obligatorio</span>
-        )}
-
-        <input
-          type="date"
-          placeholder="Fecha de Nacimiento"
-          {...register('fecha_nacimiento', { required: true })}
-          className="w-full p-4 m-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
-        />
-        {errors.fecha_nacimiento && (
-          <span className="text-red-500">
-            La fecha de nacimiento es obligatoria
-          </span>
-        )}
+        <div className="flex w-full gap-4">
+          <div className="w-1/2">
+            <label className="pl-4 text-sm"> Nombre </label>
+            <input
+              type="text"
+              placeholder="Nombre"
+              {...register('nombre', { required: true })}
+              className="w-full p-4 mx-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
+            />
+            {errors.nombre && (
+              <span className="text-red-500">El nombre es obligatorio</span>
+            )}
+          </div>
+          <div className="w-1/2">
+            <label className="pl-4 text-sm">Apellido</label>
+            <input
+              type="text"
+              placeholder="Apellido"
+              {...register('apellido', { required: true })}
+              className="w-full p-4 mx-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
+            />
+            {errors.apellido && (
+              <span className="text-red-500">El apellido es obligatorio</span>
+            )}
+          </div>
+        </div>
+        <div className="flex w-full gap-4">
+          <div className="w-7/12">
+            <label className="pl-4 text-sm"> Email </label>
+            <input
+              type="email"
+              placeholder="Email"
+              {...register('email', { required: true })}
+              className="w-full p-4 mx-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
+            />
+            {errors.email && (
+              <span className="text-red-500">El email es obligatorio</span>
+            )}
+          </div>
+          <div className="w-2/6">
+            <label className="pl-4 text-sm"> DNI </label>
+            <input
+              type="number"
+              placeholder="DNI"
+              {...register('dni', { required: true })}
+              className="w-full p-4 mx-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
+            />
+          </div>
+        </div>
+        <div className="flex w-full gap-4">
+          <div className="w-1/2">
+            <label className="pl-4 text-sm"> Número </label>
+            <input
+              type="number"
+              placeholder="Número de Teléfono"
+              {...register('numero', { required: true })}
+              className="w-full p-4 mx-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
+            />
+            {errors.numero && (
+              <span className="text-red-500">El número es obligatorio</span>
+            )}
+          </div>
+          <div className="w-1/2">
+            <label className="pl-4 text-sm"> Fecha nacimiento </label>
+            <input
+              type="date"
+              placeholder="Fecha de Nacimiento"
+              {...register('fecha_nacimiento', { required: true })}
+              className="w-full p-4 mx-2 text-left transition-all duration-500 border-b shadow-[#644fff] shadow-sm hover:shadow-lg  border-b-[#6aabffe0] h-9 placeholder-[#7a7a7a] bg-[#c9ccce] hover:bg-[#aab4bb] focus:bg-[#aab4bb] rounded-xl opacity-60 focus:shadow-md focus:shadow-[#6aabffe0] focus:outline-[#6aabffe0]"
+            />
+            {errors.fecha_nacimiento && (
+              <span className="text-red-500">
+                La fecha de nacimiento es obligatoria
+              </span>
+            )}
+          </div>
+        </div>
         <div className="flex items-center justify-center w-full mt-4">
           <button
             type="submit"
+            disabled={loading}
             className="w-3/5 h-12 m-2 text-md font-medium hover:font-bold tracking-wide hover:tracking-widest transition-all duration-700 text-white bg-[#644fff] border border-transparent rounded-lg group hover:bg-[#503ce7] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Guardar
+            {loading ? 'Cargando...' : 'Guardar'}
           </button>
         </div>
       </form>
@@ -151,7 +194,7 @@ export const EditMyProfile = ({ profile, onClose }) => {
         >
           ELIMINAR PERFIL
         </button>
-
+        <div className="m-2">{loading && <LoadingSpinner />}</div>
         <button
           onClick={onClose}
           className="w-32 h-12 m-2 text-md font-medium hover:font-bold tracking-wide hover:tracking-widest transition-all duration-700 text-white bg-[#666666c7] border border-transparent rounded-lg group hover:bg-[#424242d8] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
