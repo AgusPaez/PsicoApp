4ab46db67284019f5353410ab142c2d63cbcaa22
AgusPaez <agus.123.paez@gmail.com>
2024-11-20 11:33:38 -0300

    add filter 'today appointments'

--- src/components/Psico-components/ListAppointments.jsx
+++ src/components/Psico-components/ListAppointments.jsx
@@ -22,6 +22,8 @@ const ListAppointment = () => {
   const [selectedAppointment, setSelectedAppointment] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  // Estado para manejar el estado del checkbox
+  const [showTodayAppointments, setShowTodayAppointments] = useState(false);
 
   // function to fetch appointments
   useEffect(() => {
@@ -36,6 +38,18 @@ const ListAppointment = () => {
 
     fetchAppointments();
   }, []);
+
+  // Función para determinar si una cita es de hoy
+  const isToday = (dateString) => {
+    const today = new Date();
+    const appointmentDate = new Date(dateString);
+
+    return (
+      today.getFullYear() === appointmentDate.getFullYear() &&
+      today.getMonth() === appointmentDate.getMonth() &&
+      today.getDate() === appointmentDate.getDate()
+    );
+  };
 
   // Function to sort appointments
   const sortAppointments = (key) => {
@@ -79,11 +93,17 @@ const ListAppointment = () => {
   // fill the appointment by name, lastname, email
   const filteredAppointments = appointments.filter((appointment) => {
     const searchLower = searchTerm.toLowerCase();
-    return (
+    const matchesSearchTerm =
       appointment.nombre.toLowerCase().includes(searchLower) ||
       appointment.apellido.toLowerCase().includes(searchLower) ||
-      appointment.email.toLowerCase().includes(searchLower)
-    );
+      appointment.email.toLowerCase().includes(searchLower);
+
+    // Filtra por citas de hoy si el checkbox está activado
+    if (showTodayAppointments) {
+      return matchesSearchTerm && isToday(appointment.fecha_consulta);
+    }
+
+    return matchesSearchTerm;
   });
 
   const handleSaveNewAppointment = async (data) => {
@@ -96,21 +116,52 @@ const ListAppointment = () => {
   };
 
   return (
-    <section className="p-4 m-4">
-      <div className="pb-8 mx-8 overflow-x-auto">
-        <input
-          type="text"
-          placeholder="Buscar por nombre, apellido, email."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-1/6 p-2 mb-4 border border-gray-400 rounded-lg"
-        />
-        <button
-          onClick={() => setIsAddModalOpen(true)}
-          className="px-4 py-2 mb-4 ml-8 text-black transition-all duration-300 bg-[#846bca] rounded hover:bg-[#735cac] hover:font-semibold hover:tracking-wide"
-        >
-          Agregar Cita
-        </button>
+    <section className="p-1.5 pt-3 m-1.5 mt-3 md:p-4 md:m-4">
+      <div className="pb-8 overflow-x-auto md:mx-8">
+        <div className="grid md:flex">
+          <div className="order-2 w-full mt-4 md:mt-0 md:w-2/6 md:order-1">
+            <input
+              type="text"
+              placeholder="Buscar por nombre, apellido o email."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="w-full p-2 border border-gray-400 rounded-lg md:mb-4 md:w-11/12"
+            />
+          </div>
+          <button
+            onClick={() => setIsAddModalOpen(true)}
+            className="order-1 md:order-2 md:w-1/6 w-full px-4 py-2 mb-1 md:mb-4  text-black transition-all duration-300 bg-[#846bca] rounded hover:bg-[#735cac] hover:font-semibold hover:tracking-wide"
+          >
+            Agregar Cita
+          </button>
+        </div>
+        <div className="flex w-full gap-4 pt-2 mt-2 md:hidden">
+          <div className="absolute">
+            <span className="m-4 text-lg right-2">SOLO CITAS DE HOY</span>
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                checked={showTodayAppointments}
+                onChange={() => setShowTodayAppointments((prev) => !prev)}
+              />
+              <div className=" w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2  peer-focus:ring-blue-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute  after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#7d65bf]"></div>
+            </label>
+          </div>
+        </div>
+
+        <div className="absolute hidden gap-4 md:inline-flex right-56 top-28">
+          SOLO CITAS DE HOY
+          <label className="relative inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              className="sr-only peer"
+              checked={showTodayAppointments}
+              onChange={() => setShowTodayAppointments((prev) => !prev)}
+            />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2  peer-focus:ring-blue-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#7d65bf]"></div>
+          </label>
+        </div>
 
         <h2 className="mt-8 mb-6 text-2xl font-bold text-center text-gray-800">
           Lista de citas
